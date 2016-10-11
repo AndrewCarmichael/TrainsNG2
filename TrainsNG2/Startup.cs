c87156ffe2986ec3e3c77db657e34ff0731f209c
andre <andre@FS4>
2016-10-11 09:22:32 +0100

    Add project files.

---
+++ TrainsNG2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TrainsNG2.Startup))]
+namespace TrainsNG2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
